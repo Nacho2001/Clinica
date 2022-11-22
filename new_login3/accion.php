18c01f11eb785b70e69b28db80929d662d4acca1
Nacho2001 <nachomigoni@gmail.com>
2022-11-22 11:04:51 -0300

    createVoucher tst1

--- new_login3/accion.php
+++ new_login3/accion.php
@@ -1,4 +1,6 @@
 <?php
+use \RouterOS\Client;
+use \RouterOS\Query;
 //Recibe los datos del formulario login_trial
 $email = $_POST['email'];
 $backlink = $_POST['backlink'];
@@ -16,12 +18,32 @@ if ( $email != "" ) { // Revisa si el campo email se encuentra vacio, continua c
     $conectar = conn();
     $sql = "insert into login(email) value ('$email')"; // Consulta SQL para ingresar el $email
     $result = mysqli_query($conectar, $sql)or trigger_error("Fallo la peticion, error sql:".mysqli_error($conectar)); // Ejecuta la consulta, si hay error muestra el mensaje
-    // Envia email con codigo de voucher
-    $destinatario = $email;
-    $asunto = "Codigo de voucher";
-    $mensaje = "Bienvenido a Clínica Pasteur! Su código de voucher es: ";
-    mail($destinatario,$asunto,$mensaje);
 
+    
+    $client = new Client([
+        'host' => '10.1.56.1',
+        'user' => 'autoCreate',
+        'pass' => 'login&mikrotik'
+    ]);
+    /*
+    function crearVoucher(){
+        $key = '';
+        $pattern = '1234567890abcdefghijlkmnopqrstuvwxyz';
+        $max = strlen($pattern)-1;
+        for($i=0;$i < 5;$i++) $key .=$pattern{mt_rand(0,$max)};
+        return $key;
+    }*/
+    
+    //$voucher = crearVoucher();
+    
+    $query = (new Query('ip/hotspot/users/add'))
+        ->equal('user','user_test')
+        ->equal('server', 'H-cp')
+        ->equal('profile','hotspot-trial')
+        ->equal('limit-uptime','');
+    
+    $crear = $client->query($query);
+    var_dump($crear);
 }
 
 header("Location: $backlink"); // Al final, redirige al mk
