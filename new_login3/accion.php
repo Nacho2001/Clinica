1ce6cba843902963d0d5b3db188d01beacacf4fc
Nacho2001 <nachomigoni@gmail.com>
2022-11-25 10:30:50 -0300

    Intento de email 1

--- new_login3/accion.php
+++ new_login3/accion.php
@@ -1,8 +1,15 @@
 <?php
+//Incluir librerias
 include_once('./lib/routeros_api.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require '.lib/PHPMailer/src/Exception.php';
+require '.lib/PHPMailer/src/PHPMailer.php';
+require '.lib/PHPMailer/src/SMTP.php';
 //Recibe los datos del formulario login_trial
-$email = $_POST['email'];
-$backlink = $_POST['backlink'];
+$email = $_POST['email']; //email
+$backlink = $_POST['backlink']; //link de retorno
 
 //Conexion con la base
 function conn(){ 
@@ -13,11 +20,33 @@ function conn(){
     $conectar = mysqli_connect($hostname, $userdb, $passworddb, $dbname); // Conexion con la base
     return $conectar;
 }
-function crearVoucher(){
+function crearVoucher(){ // Genera código de voucher (aleatorio)
     $caracteres = "0123456789abcdefghijklmnopqrstuvwxyz";
-    return substr(str_shuffle($caracteres),0,5);
+    return substr(str_shuffle($caracteres),0,6);
 }
+$Vcode = crearVoucher(); // Codigo guardado en variable
 
+function email(){
+    $mail = new PHPMailer(true);
+    //Ajustes server
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    //$mail->isSMTP(); // Utilizar SMTP
+    //$mail->Host = 'smtp.example.com'; //SMTP al que se enviara
+    //$mail->SMTPAuth = true;   //Habilita autenticacion SMTP
+    //$mail->Username = ''; //SMTP nombre de usuario
+    //$mail->Password = '';   //SMTP contraseña
+    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;    //Habilita encriptacion
+    //$mail->Port = 465;    // Puerto
+    //Email remitente y destinatario
+    $mail->setFrom('no-reply@cpasteur.com.ar'); // Remitente
+    $mail->addAddress('nachomigoni@gmail.com'); // Destinatario
+    //Contenido de email
+    $mail->isHTML(true); // Formato html para el contenido
+    $mail->Subject = 'Voucher code'; // Asunto
+    $mail->Body = "Código de voucher: $Vcode"; // Cuerpo
+    $mail->send(); // Envia
+
+}
 function voucher(){
     // Invoca la api RouterOS
     $api = new RouterosAPI();
@@ -32,14 +61,15 @@ function voucher(){
         $api->comm("/ip/hotspot/user/add", array(
             //Datos Voucher a crear
             "server" => "H-cp",  // Hotspot donde se utilizará el voucher
-            "name" => "testingAuto", // codigo de voucher (usuario)
-            "password" => "testingAuto", // contraseña de voucher (mismo valor que código)
+            "name" => "$Vcode", // codigo de voucher (usuario)
+            "password" => "$Vcode", // contraseña de voucher (mismo valor que código)
             "profile" => "Invitados", // Perfil de voucher
             "limit-uptime" => "8h", // Tiempo del voucher
             "comment" => "Invitado registrado", // Comentario
         ));
     }
 }
+// Inicio del programa
 if ( $email != "" ) { // Revisa si el campo email se encuentra vacio, continua con el proceso
     $conectar = conn();
     $sql = "insert into login(email) value ('$email')"; // Consulta SQL para ingresar el $email
@@ -47,6 +77,6 @@ if ( $email != "" ) { // Revisa si el campo email se encuentra vacio, continua c
     voucher();
 }
 
-header("Location: $backlink"); // Al finalm redirige al mikrotik
+header("Location: $backlink"); // Al final, redirige al mikrotik
 exit();
 ?>
