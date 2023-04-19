27758588dc8219c04a3d6c0b275389c70977b1b1
Nacho2001 <nachomigoni@gmail.com>
2023-04-19 09:09:35 -0300

    revisar error

--- if_iperf6/index.php
+++ if_iperf6/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once "funciones.php"?>
+<?php require_once "main.php"?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,22 +8,47 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="post" id="switches">
+    <form action="index.php" method="post">
         <div>
             <h3>Seleccinar switches</h3>
-            <select name="switch1" id="switch">
+            <select name="switch1">
                 <option value="0">switch</option>
                 <?php host($conn);?>
             </select>
-            <select name="switch2" id="switch" style="margin-top: 5px">
+            <select name="switch2" style="margin-top: 5px">
                 <option value="0">switch</option>
                 <?php host($conn);?>
             </select>
             <button style="margin-top: 5px;">Cargar</button>
         </div>
     </form>
+    <button style="margin-top: 5px" onclick="grafico()">Ver grafico</button>
     <div style="background-color: orange; ">
         <canvas id="chart" hidden></canvas>
     </div>
 </body>
+<script>
+    function grafico(){
+        document.getElementById("chart").innerHTML = 
+        `<canvas id="chart" style="width:250px"></canvas>`;
+        new Chart("chart", {
+            type: "line",
+            data: {
+                labels: [<?php for($i=0;$i<count($array_X);$i++){echo $array_X[$i].',';} ?>],
+                datasets: [{
+                    data: [<?php for($i=0;$i<count($array_Y1);$i++){echo $array_Y1[$i].',';} ?>],
+                    borderColor: "green",
+                    fill: false
+                },{
+                    data: [<?php for($i=0;$i<count($array_Y2);$i++){echo $array_Y2[$i].',';} ?>],
+                    borderColor: "red",
+                    fill: false
+                }]
+            },
+            options: {
+                legend: {display: false}
+            }
+        });
+    }
+</script>
 </html>
--- if_iperf6/main.php
+++ if_iperf6/main.php
@@ -16,4 +16,29 @@ function host($conexion){
 };
 
 // Crear arrays con los valores x y el value y
+function valoresX($enlace){
+    $resp = mysqli_query($enlace, "select distinct dia from diario_ps");
+    while($column = mysqli_fecht_row($resp)){
+        $array_X[] = json_encode($column[0]);
+    }
+}
+
+function valoresY($enlace,$switch){
+    $resp = mysqli_query($enlace, 'select medido from diario_ps where host = "'.$switch.'"');
+    while($column = mysqli_fecht_row($resp)){
+        $array_Y[] = json_encode($column[0]);
+    }
+    return $array_Y;
+}
+/*
+if($_SERVER["REQUEST METHOD"] == "POST"){
+    $array_X = array();
+    $array_Y1 = array();
+    $array_Y2 = array();
+    $switch1 = $_POST["switch1"];
+    $switch2 = $_POST["switch2"];
+    valoresY($conn);
+    $array_Y1 = valoresY($conn,$switch1);
+    $array_Y2 = valoresY($conn,$switch2);
+}*/
 ?>
