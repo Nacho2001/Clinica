aaaef81b2b81154c4bdd7d12b30f3ae485c3e664
Nacho2001 <nachomigoni@gmail.com>
2023-06-08 15:32:09 -0300

    Select finalizado

--- if_interface2/src/components/Graficos.vue
+++ if_interface2/src/components/Graficos.vue
@@ -2,43 +2,48 @@
     <div>
         <h3>Seleccionar Switches</h3>
         <div>
-            <select id="switch">
-                <!--<option v-for="host of lista" v-bind:key="host.switch" v-bind:value="host.switch">{{host.switch}}</option>-->
-            </select>
-            <div id="result"></div>
+            <select id="switch"></select>
+            <button style="margin-left: 1%;" @click="getSwitch()">Añadir</button>
+            <div style="margin-top: 1%;">
+                Switches seleccionados: <a id="switchList"></a>
+            </div>
         </div>
     </div>
 </template>
 <script>
 const axios = require('axios');
-    axios.get('http://localhost:9770/switches/')
-    .then(function(response){
-        let obe=response.data.data[1].switch
-        console.log(obe)
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+
 export default {
-    name:'GraficosComponent'/*,
-    data(){
-        return{
-            lista:[]
-        }
-    },
-    methods:{
-        obtenerListaSwitches(){
+    name:'GraficosComponent',
+    methods: {
+        obtenerSwitches(){
             axios.get('http://localhost:9770/switches/')
             .then(function(response){
-                lista = response.data;
+                let lista=response.data.data
+                lista.forEach(objeto => {
+                    let option = document.createElement("option")
+                    option.text = objeto.switch,
+                    option.value = objeto.switch
+                    document.getElementById("switch").appendChild(option)
+                });
             })
             .catch(function(error){
                 console.log(error)
             })
+        },
+        getSwitch(){
+            let host = document.getElementById("switch").value
+            document.getElementById("switchList").append(host)
+            /*seleccionados.push(host)
+            seleccionados.forEach(element => {
+                console.log(element)
+            });*/
         }
     },
     mounted(){
-        this.obtenerListaSwitches()
-    }*/
+        this.obtenerSwitches()
+        /*let seleccionados = []*/
+
+    }
 }
 </script>
