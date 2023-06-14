19f188ca93cfe2053c048639b366faf8a9aa16ca
Nacho2001 <nachomigoni@gmail.com>
2023-06-14 15:14:40 -0300

    corregir ObtenerY

--- if_interface2/src/components/Graficos.vue
+++ if_interface2/src/components/Graficos.vue
@@ -2,11 +2,14 @@
     <div>
         <h3>Seleccionar Switches</h3>
         <div>
-            <select id="switch"></select>
+            <select id="switch">
+                <option v-for="element of listaFull" v-bind:key="element.switch" v-bind:value="element.switch">{{element.switch}}</option>
+            </select> 
             <button style="margin-left: 1%;" @click="getSwitch()">Añadir</button>
             <div style="margin-top: 1%;">
                 Switches seleccionados: <a id="switchList"></a>
             </div>
+            <button @click="obtenerY()">Ver elegidos</button>
         </div>
     </div>
 </template>
@@ -15,35 +18,65 @@ const axios = require('axios');
 
 export default {
     name:'GraficosComponent',
+    data(){
+        return {
+            listaFull:[],
+            mySwitches:[],
+            valuesX:[],
+            valuesY:[]
+        }
+    },
     methods: {
         obtenerSwitches(){
             axios.get('http://localhost:9770/switches/')
-            .then(function(response){
-                let lista=response.data.data
+            /*.then(function(response){
+                this.listaFull=response.data.data
                 lista.forEach(objeto => {
                     let option = document.createElement("option")
                     option.text = objeto.switch,
                     option.value = objeto.switch
                     document.getElementById("switch").appendChild(option)
                 });
+            })*/
+            .then(result => {
+                this.listaFull = result.data.data
             })
             .catch(function(error){
                 console.log(error)
             })
         },
+        obtenerX(){
+            axios.get('http://localhost:9770/switches/dataX')
+            .then(result => { 
+                let objetoFechas = result.data.data
+                for (let i = 0; i < objetoFechas.length; i++) {
+                    this.valuesX.push(objetoFechas[i])
+                }
+            })
+        },
+        obtenerY(){
+            for (let i1 = 0; i1 < this.mySwitches.length; i1++) {
+                let Y$i1 = []
+                axios.get(`http://localhost:9770/switches/dataY/${this.mySwitches[i1]}`)
+                .then(result => {
+                    let objetoDatoY = (result.data.data)
+                    for (let i2 = 0; i2 < objetoDatoY.length; i2++) {
+                        Y$i1.push(objetoDatoY[i2].medido)
+                        
+                    }
+                })
+                Y$i1.push(this.valuesY)
+                console.log(this.valuesY)
+            }
+        },
         getSwitch(){
             let host = document.getElementById("switch").value
-            document.getElementById("switchList").append(host)
-            /*seleccionados.push(host)
-            seleccionados.forEach(element => {
-                console.log(element)
-            });*/
+            document.getElementById("switchList").append(`${host} `)
+            this.mySwitches.push(host);
         }
     },
     mounted(){
         this.obtenerSwitches()
-        /*let seleccionados = []*/
-
     }
 }
 </script>
